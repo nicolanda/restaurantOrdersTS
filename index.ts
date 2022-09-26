75d93229f1ca0fe6872a634a62251fee0485ebf6
nicolanda <nicolaslandazabal@hotmail.com>
2022-09-26 14:58:05 -0500

    second commit

---
+++ index.ts
@@ -0,0 +1,43 @@
+import { restaurants, Restaurant } from "./restaurants";
+import { orders, Order, PriceBracket } from "./orders";
+
+/// Add your getMaxPrice() function below:
+const getMaxPrice = (price: PriceBracket) =>{
+  switch(price){
+    case PriceBracket.Low:
+      return 10.0;
+    case PriceBracket.Medium:
+      return 20.0;
+    case PriceBracket.High:
+      return 30.0;
+  }
+}
+/// Add your getOrders() function below:
+
+const getOrders = (price: PriceBracket, orders:Order[][] ) => {
+  let filteredOrders: Order[][] = [];
+  orders[0].filter((order: Order) => order.price <= getMaxPrice(price));
+
+  orders.forEach((restaurant: Order[]) => {
+    const resutl = restaurant.filter((order: Order) => order.price <= getMaxPrice(price));
+    filteredOrders.push(resutl);
+  });
+  return filteredOrders;
+}
+/// Add your printOrders() function below:
+
+const printOrders = (restaurants: Restaurant[], orders: Order[][]) => {
+  restaurants.forEach((restaurant: Restaurant, index: number) => {
+    if(orders[index].length > 0){
+      console.log(restaurant.name);
+      orders[index].forEach(order => {
+        console.log(`- ${order.name} (${order.price})`);
+      })
+    }
+  });
+}
+
+/// Main
+const elligibleOrders = getOrders(PriceBracket.Low, orders);
+// console.log(elligibleOrders)
+printOrders(restaurants, elligibleOrders);
